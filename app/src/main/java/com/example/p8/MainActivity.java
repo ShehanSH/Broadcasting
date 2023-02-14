a034ca46065483468a7b65f63917c311bc95f3b9
ShehanSH <shehanhashen928@gmail.com>
2023-02-15 00:01:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/p8/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.p8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    airplane.AirplaneModeChangeReceiver airplaneModeChangeReceiver = new airplane.AirplaneModeChangeReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(airplaneModeChangeReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(airplaneModeChangeReceiver);
+    }
+
+
+
+}
